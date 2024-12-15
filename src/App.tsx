ed2aa366330309bf8f47cf5c33450e90b073df75
Alvaro Iglesias <83364924+alvaroid01@users.noreply.github.com>
2024-12-15 22:37:20 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Configurator } from "./features/Configurator/Configurator";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Configurator />
+    </div>
+  );
+}
